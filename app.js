f6e5c229931210fa75c8067a9bc64046ef57fc5e
yusufakbs <yusufakbas47@hotmail.com>
2021-12-29 02:31:25 +0300

    UsersPageDesign

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let mainWindow;
 
 app.on('ready',()=>{
     mainWindow = new BrowserWindow({
+        width: 940, height:600 ,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
